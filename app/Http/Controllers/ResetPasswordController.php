1e89d1386c084c6ee460642fb415c363c5d5819b
heyosseus <ratiruxadzee@gmail.com>
2023-04-13 20:49:45 +0400

    wip: add controller to handle reset password and email confirmation

---
+++ app/Http/Controllers/ResetPasswordController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\View\View;
+
+class ResetPasswordController extends Controller
+{
+    public function create() : View
+    {
+        return view('verification.reset-password');
+    }
+}

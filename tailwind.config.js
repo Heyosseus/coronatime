6593a91f9fd9868ede8ac6964708ea4f4064218c
heyosseus <ratiruxadzee@gmail.com>
2023-04-13 14:21:26 +0400

    feat: add input-form

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,10 @@ module.exports = {
         },
     },
   },
-  plugins: [],
+  plugins: [
+      require('@tailwindcss/forms')({
+          strategy: 'class', // only generate classes
+      }),
+  ],
 }
 

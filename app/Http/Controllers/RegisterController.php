06f65f4b86a6fe0aa217b733a5f9f84c0401b59c
heyosseus <ratiruxadzee@gmail.com>
2023-04-14 12:52:55 +0400

    feat: add cs fixer configuration

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -5,33 +5,32 @@ namespace App\Http\Controllers;
 use App\Http\Requests\StoreRegisterRequest;
 use App\Models\User;
 use Illuminate\Http\RedirectResponse;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 
 class RegisterController extends Controller
 {
-    public function create()
-    {
-        return view('auth.register');
-    }
+	public function create()
+	{
+		return view('auth.register');
+	}
 
-    public function store(StoreRegisterRequest $request): RedirectResponse
-    {
-        $attributes =  $request->validated();
+	public function store(StoreRegisterRequest $request): RedirectResponse
+	{
+		$attributes = $request->validated();
 
-        $user = User::create([
-            'name' => $attributes['name'],
-            'email' => $attributes['email'],
-            'password' => Hash::make($attributes['password']),
-        ]);
+		$user = User::create([
+			'name'     => $attributes['name'],
+			'email'    => $attributes['email'],
+			'password' => Hash::make($attributes['password']),
+		]);
 
-        // Check if the "remember_device" checkbox was checked
-        $rememberDevice = $request->has('remember_device');
+		// Check if the "remember_device" checkbox was checked
+		$rememberDevice = $request->has('remember_device');
 
-        // Store the value of the checkbox in the "users" table
-        $user->remember_device = $rememberDevice;
-        $user->save();
+		// Store the value of the checkbox in the "users" table
+		$user->remember_device = $rememberDevice;
+		$user->save();
 
-        return redirect('/')->with('success', 'Your account has been created!');
-    }
+		return redirect('/')->with('success', 'Your account has been created!');
+	}
 }

cb246fb40ef48c68bac1ccf406209e63131e8fd7
heyosseus <ratiruxadzee@gmail.com>
2023-04-14 15:51:57 +0400

    feat: add destroy method for logut

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -4,30 +4,35 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\StoreLoginRequest;
 use Illuminate\Http\RedirectResponse;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
 use Illuminate\Validation\ValidationException;
 
 class LoginController extends Controller
 {
-    public function create()
-    {
-        return view('auth.login');
-    }
+	public function create()
+	{
+		return view('auth.login');
+	}
 
-    public function store(StoreLoginRequest $request): RedirectResponse
-    {
-        $attributes = $request->validated();
+	public function store(StoreLoginRequest $request): RedirectResponse
+	{
+		$attributes = $request->validated();
 
-        if (! auth()->attempt($attributes)) {
-            throw ValidationException::withMessages([
-                'name' => 'Your provided credentials could not be verified.'
-            ]);
-        }
-        session()->regenerate();
+		if (!auth()->attempt($attributes)) {
+			throw ValidationException::withMessages([
+				'name' => 'Your provided credentials could not be verified.',
+			]);
+		}
+		session()->regenerate();
 
-        session()->flash('status', 'Welcome Back!');
+		session()->flash('status', 'Welcome Back!');
 
-        return redirect()->route('home');
-    }
+		return redirect()->route('home');
+	}
+
+	public function destroy(): RedirectResponse
+	{
+		auth()->logout();
+
+		return redirect()->route('home')->with('success', 'Goodbye!');
+	}
 }

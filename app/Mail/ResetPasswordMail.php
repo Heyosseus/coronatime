832f3a5d5ab6ed95b869ee5072ee4fa3d12b04c6
heyosseus <ratiruxadzee@gmail.com>
2023-04-15 00:55:07 +0400

    chore: minor updates

---
+++ app/Mail/ResetPasswordMail.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+
+class ResetPasswordMail extends Mailable
+{
+	use Queueable, SerializesModels;
+
+	/**
+	 * Create a new message instance.
+	 */
+	public function __construct($token)
+	{
+		$this->token = $token;
+	}
+
+	public function build(): ResetPasswordMail
+	{
+		$token = route('email_verification_reset_password', ['token' => $this->token]);
+
+		return $this->view('emails.confirm-reset-password')
+			->subject('Reset your password')
+			->with(['token' => $this->token]);
+	}
+}

d98c0480f8ebcc721308b0034da530c4da5b2209
heyosseus <ratiruxadzee@gmail.com>
2023-04-16 00:55:05 +0400

    chore: modify translations

---
+++ resources/lang/en/login.php
@@ -0,0 +1,15 @@
+<?php
+
+return[
+	'welcome'          => 'Welcome back',
+	'paragraph'        => 'Welcome back! Please enter your details',
+	'username'         => 'Username',
+	'password'         => 'Password',
+	'remember'         => 'Remember this device',
+	'forgot'           => 'Forgot password?',
+	'login'            => 'Log in',
+	'account'          => 'Don\'t have an account?',
+	'sign_up'          => 'Sign up for free',
+	'enter_password'   => 'Fill in password',
+	'enter_username'   => 'Enter unique username or email',
+];
---
+++ resources/lang/en/register.php
@@ -0,0 +1,18 @@
+<?php
+
+return[
+	'title'            => 'Welcome to Coronatime',
+	'paragraph'        => 'Please enter required info to sign up',
+	'username'         => 'Username',
+	'email'            => 'Email',
+	'password'         => 'Password',
+	'repeat'           => 'Repeat Password',
+	'remember'         => 'Remember this device',
+	'account'          => 'Already have an account?',
+	'sign_up'          => 'Sign Up',
+	'login'            => 'Log in',
+	'enter_name'       => 'Enter unique username',
+	'enter_email'      => 'Enter your email',
+	'enter_password'   => 'Fill in password',
+	'enter_repeat'     => 'Repeat  password',
+];

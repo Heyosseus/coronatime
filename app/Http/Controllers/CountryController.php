5b87a8c5abe87210f7f2090209048929abdd2b8c
heyosseus <ratiruxadzee@gmail.com>
2023-04-21 17:23:06 +0400

    wip: add sorting funcionality

--- app/Http/Controllers/CountryController.php
+++ app/Http/Controllers/CountryController.php
@@ -4,14 +4,48 @@ namespace App\Http\Controllers;
 
 use App\Models\Country;
 
+//class CountryController extends Controller
+//{
+//	public function index()
+//	{
+//		$countries = Country::first()->get();
+//		if (request('search')) {
+//			$countries = Country::where('location', 'like', '%' . request('search') . '%')->get();
+//		}
+//		return view('country', compact('countries'));
+//	}
+//}
 class CountryController extends Controller
 {
 	public function index()
 	{
-		$countries = Country::latest()->get();
-		if (request('search')) {
-			$countries = Country::where('location', 'like', '%' . request('search') . '%')->get();
+		$sortBy = request('sort_by', 'location');
+		$sortOrder = request('sort_order', 'asc');
+
+		// Determine the new sort order to be used for the sorting buttons
+		$newSortOrder = ($sortOrder === 'asc') ? 'desc' : 'asc';
+
+		// Retrieve the countries from the database, sorted by the selected column and order
+		$query = Country::query();
+		switch ($sortBy) {
+			case 'new_cases':
+				$query->orderBy('new_cases', $sortOrder);
+				break;
+			case 'recovered':
+				$query->orderBy('recovered', $sortOrder);
+				break;
+			case 'deaths':
+				$query->orderBy('deaths', $sortOrder);
+				break;
+			default:
+				$query->orderBy('location', $sortOrder);
 		}
-		return view('country', compact('countries'));
+		if (request('search')) {
+			$query->where('location', 'like', '%' . request('search') . '%');
+		}
+		$countries = $query->get();
+
+		// Pass the data to the view
+		return view('country', compact('countries', 'sortBy', 'sortOrder', 'newSortOrder'));
 	}
 }

d0823231c0acdaffe9cf81247ef9ca2c166a739e
heyosseus <ratiruxadzee@gmail.com>
2023-04-22 18:07:57 +0400

    style: remove paddings

--- resources/views/country.blade.php
+++ resources/views/country.blade.php
@@ -1,14 +1,14 @@
 <x-dashboard-layout>
     <div class="p-6">
-        <h1 class="font-extrabold text-xl lg:text-3xl lg:px-16">@lang('home.statistics')</h1>
-        <div class="flex space-x-6 mt-5 lg:mt-10 lg:px-16
+        <h1 class="font-extrabold text-xl lg:text-3xl ">@lang('home.statistics')</h1>
+        <div class="flex space-x-6 mt-5 lg:mt-10
         ">
             <a href="{{route('home')}}" class="lg:text-xl">@lang('home.worldwide')</a>
             <a href="#" class="font-bold relative lg:text-xl"> @lang('home.by_country')
                 <div class="absolute bg-gray-950 mt-3 ml-1 h-1 w-20 lg:w-24"></div>
             </a>
         </div>
-        <div class="lg:px-16">
+        <div class="">
             <form action="#" method="GET"
                   class=" h-14 rounded-xl py-2 lg:px-6 lg:border lg:border-gray-300 outline-none bg-white flex items-center  mt-10 lg:w-80 ">
                 <img src="/storage/search.png" alt="" class="w-6 h-6">
@@ -33,7 +33,7 @@
     @endforeach
     <div>
         <div class="overflow-y-auto max-h-[55vh] w-fit scrollbar-thumb-[#808189] scrollbar-track-gray-200 scrollbar ">
-            <table class="table w-full bg-gray-50 px-4 text-sm h-full lg:mx-auto lg:ml-20  lg:text-lg lg:w-[1240px]">
+            <table class="table w-full bg-gray-50 px-4 text-sm h-full lg:mx-auto lg:ml-6 lg:text-lg lg:w-[1240px]">
                 <thead>
                 <tr class="bg-gray-100 ">
                     <th class="w-1/4 px-2 py-2 text-left lg:py-4 lg:px-3 ">
